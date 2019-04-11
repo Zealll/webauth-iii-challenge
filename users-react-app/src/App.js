f9890ce0c3a725413fb1c1b56bddb7796087c203
Zealll <eriznis@gmail.com>
2019-04-11 15:48:27 -0400

    Login functionality added

--- users-react-app/src/App.js
+++ users-react-app/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Route } from 'react-router-dom'
+
+import Login from './auth/Login'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          
-        </header>
+        <Route exact path='/' component={Login} />
       </div>
     );
   }
--- users-react-app/src/index.js
+++ users-react-app/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter as R} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<R><App /></R>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
